da660ae0a375a28ef9831c6fa260575fa6c40a39
pknell <pknell@nvisia.com>
2018-06-18 13:15:40 -0500

    adding Terraform file and the IAM policy file

---
+++ start-stop-environment.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_iam_policy" "manage_environment_iam_policy" {
+  name = "ManageEnvPolicy"
+  policy = "${file("manage-environment-policy.json")}"
+}
+
+resource "aws_iam_role" "manage_environment_iam_role" {
+  name = "ManageEnvRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "manage_environment_iam_policy_attachment" {
+    name = "ManageEnvPolicyAttachment"
+    policy_arn = "${aws_iam_policy.manage_environment_iam_policy.arn}"
+    roles = ["${aws_iam_role.manage_environment_iam_role.name}"]
+}
+
+resource "aws_lambda_function" "start_environment_lambda" {
+  filename         = "start_env_lambda/start_environment_lambda_payload.zip"
+  function_name    = "StartEnvironment"
+  role             = "${aws_iam_role.manage_environment_iam_role.arn}"
+  handler          = "index.handler"
+  source_code_hash = "${base64sha256(file("start_env_lambda/start_environment_lambda_payload.zip"))}"
+  runtime          = "nodejs8.10"
+  memory_size      = 128
+  timeout          = 15
+}
+
+resource "aws_lambda_function" "stop_environment_lambda" {
+  filename         = "stop_env_lambda/stop_environment_lambda_payload.zip"
+  function_name    = "StopEnvironment"
+  role             = "${aws_iam_role.manage_environment_iam_role.arn}"
+  handler          = "index.handler"
+  source_code_hash = "${base64sha256(file("stop_env_lambda/stop_environment_lambda_payload.zip"))}"
+  runtime          = "nodejs8.10"
+  memory_size      = 128
+  timeout          = 15
+}
+
+resource "aws_cloudwatch_event_rule" "start_environment_rule" {
+  name                = "StartEnvironmentRule"
+  schedule_expression = "cron(30 14 ? * 2-6 *)"
+}
+
+resource "aws_cloudwatch_event_rule" "stop_environment_rule" {
+  name                = "StopEnvironmentRule"
+  schedule_expression = "cron(0 15 ? * 2-6 *)"
+}
